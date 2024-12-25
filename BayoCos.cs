67bf74f6655929aedc4c756440845e799fd082c7
Lyzder <santiagot791@gmail.com>
2024-12-25 14:29:48 -0500

    Add project files.

---
+++ BayoCos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayoCosTool
+{
+    public class CosHeader
+    {
+        uint size;
+        uint version;
+        uint offsetEntries;
+        uint numEntries;
+
+        public CosHeader(uint size, uint version, uint offsetEntries, uint numEntries)
+        {
+            this.size = size;
+            this.version = version;
+            this.offsetEntries = offsetEntries;
+            this.numEntries = numEntries;
+        }
+
+        public uint GetOffsets()
+        {
+            return offsetEntries;
+        }
+
+        public uint GetNumOfEntries()
+        {
+            return numEntries;
+        }
+    }
+
+    public class CosColor
+    {
+        public float Red { get; set; }
+        public float Green { get; set; }
+        public float Blue { get; set; }
+
+        public CosColor(float red, float green, float blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+
+    public class CosEntry
+    {
+        CosColor[] colors;
+        char[] name;
+
+        public CosEntry()
+        {
+            colors = new CosColor[8];
+            name = new char[64];
+        }
+    }
+
+    public class Cos
+    {
+        CosHeader header;
+        CosHeader? header2;
+        CosEntry[] entries;
+
+        public Cos(CosHeader header)
+        {
+            this.header = header;
+            entries = new CosEntry[8];
+        }
+
+        public void SetHeader2(CosHeader header2)
+        {
+            this.header2 = header2;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BayoCosTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private MainWindowViewModel _viewModel;
+        private string file_name = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Nullable<bool> result;
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.InitialDirectory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            dlg.Filter = "Cos file (*.cos)|*.cos|All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
+
+            result = dlg.ShowDialog();
+            // User didn't select a file so don't do anything
+            if (result == false)
+                return;
+
+            // Load the file the user selected  
+            Console.WriteLine(dlg.FileName);
+
+            return;
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            return;
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveAsCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
